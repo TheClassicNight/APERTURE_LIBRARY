fdfed63f8de8e41bf1510238520f86ffd99b5b60
alex Espinoza Castillo <alex.espinoza@hotmail.com.ar>
2023-03-17 06:38:09 -0700

    ENTREGABLE 1

--- APERTURE_LIBRARY/Controllers/HomeController.cs
+++ APERTURE_LIBRARY/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using APERTURE_LIBRARY.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace APERTURE_LIBRARY.Controllers
 {
     public class HomeController : Controller
     {
+        LibraryAPEntities db = new LibraryAPEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +28,62 @@ namespace APERTURE_LIBRARY.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Books()
+        {
+            var lst = db.Libros.ToList();
+            return View(lst);
+        }
+
+        public ActionResult BooksForm()
+        {
+            var ID = Request.Params["IdLibro"];
+            if (ID != null)
+            {
+                int id = int.Parse(ID);
+                var li = db.Libros.Where(x => x.IdLibro == id).FirstOrDefault();
+                ViewBag.li = li;
+            }
+            return View();
+        }
+
+        public JsonResult guardarLi(int? IdLibro, string NombreLibro, string Autor, string Editorial, string FPublicacion, float CostoLibros, int CantidadLibros, int NoPaginas)
+        {
+            if (IdLibro != null)
+            {
+                var Art = db.Libros.Where(x => x.IdLibro == IdLibro).FirstOrDefault();
+                Art.NombreLibro = NombreLibro;
+                Art.Autor = Autor;
+                Art.Editorial = Editorial;
+                Art.FPublicacion = FPublicacion;
+                Art.CostoLibros = CostoLibros;
+                Art.CantidadLibros = CantidadLibros;
+                Art.NoPaginas = NoPaginas;
+                db.SaveChanges();
+            }
+            else
+            {
+                Libros Art = new Libros();
+                Art.NombreLibro = NombreLibro;
+                Art.Autor = Autor;
+                Art.Editorial = Editorial;
+                Art.FPublicacion = FPublicacion;
+                Art.CostoLibros = CostoLibros;
+                Art.CantidadLibros = CantidadLibros;
+                Art.NoPaginas = NoPaginas;
+                db.Libros.Add(Art);
+                db.SaveChanges();
+            }
+            return Json("");
+        }
+
+        public ActionResult Eliminar(int? IdLibro)
+        {
+            var book = db.Libros.Where(x => x.IdLibro == IdLibro).FirstOrDefault();
+            db.Libros.Remove(book);
+            db.SaveChanges();
+            return RedirectToAction("Books", "home");
         }
     }
 }
---
+++ APERTURE_LIBRARY/Scripts/js_Books.js
@@ -0,0 +1,40 @@
+﻿function getParameterByName(name) {
+    if (name !== "" && name !== null && name != undefined) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+            results = regex.exec(location.search);
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    } else {
+        var arr = location.href.split("/");
+        return arr[arr.length - 1];
+    }
+
+}
+
+function guardarLi() {
+    var id = getParameterByName("IdLibro");
+    $.ajax({
+        type: "POST",
+        url: UrlGuardarLi,
+        async: true,
+        data: {
+
+            IdLibro: id,
+            NombreLibro: document.getElementById("nombre").value,
+            Autor: document.getElementById("autor").value,
+            Editorial: document.getElementById("edit").value,
+            FPublicacion: document.getElementById("fecha").value,
+            CostoLibros: document.getElementById("costo").value,
+            CantidadLibros: document.getElementById("cantidad").value,
+            NoPaginas: document.getElementById("paginas").value,
+
+        },
+        success: function (data) {
+
+            location.href = "../home/Books";
+        },
+        error: function (xhr, status, error) {
+            alert(error);
+        }
+    });
+}
